82040583360ef2f8fb216de230dd8d05e8d72941
MDLicht <zombie612z@gmail.com>
2021-10-04 01:59:09 +0900

    * SplashActivity To MainActivity

--- app/src/main/java/com/mdlicht/navpractice/SplashActivity.kt
+++ app/src/main/java/com/mdlicht/navpractice/SplashActivity.kt
@@ -1,11 +1,31 @@
 package com.mdlicht.navpractice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.navigation.ActivityNavigator
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.addTo
+import java.util.concurrent.TimeUnit
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Observable.timer(3, TimeUnit.SECONDS)
+            .doOnComplete {
+                moveToMainActivity()
+            }
+            .subscribe()
+            .addTo(compositeDisposable)
+    }
+
+    private fun moveToMainActivity() {
+        val activityNavigator = ActivityNavigator(this)
+        activityNavigator.navigate(
+            activityNavigator.createDestination()
+                .setIntent(Intent(this, MainActivity::class.java)),
+            null, null, null
+        )
     }
 }
